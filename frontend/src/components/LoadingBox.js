86bb2259677e6691a7675668010a0a55ccaf237e
weijang <weijang.lim@gmail.com>
2024-08-04 22:49:44 +0800

    remove

--- frontend/src/components/LoadingBox.js
+++
@@ -1,9 +0,0 @@
-import Spinner from "react-bootstrap/Spinner";
-
-export default function LoadingBox () {
-    return (
-    < Spinner animation="boder" role="status">
-    <span className="visually-hidden">LoadingBox...</span>
-    </Spinner>
-    );
-}
--- frontend/src/components/MessageBox.js
+++
@@ -1,7 +0,0 @@
-import Alert from "react-bootstrap/Alert";
-
-export default function MessageBoxBox (props) {
-    return (
-        <Alert variamt={props.variamt || 'info'}>{props.children}</Alert>
-    );
-}

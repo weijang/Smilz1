77802c662a3a645ddabb6c4da9e8531d997a942f
weijang <weijang.lim@gmail.com>
2024-08-09 22:47:06 +0800

    update V36

--- frontend/src/components/AdminRoute.js
+++ frontend/src/components/AdminRoute.js
@@ -2,8 +2,8 @@ import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { Store } from '../components/Store';
 
-export default function ProtectedRoute({ children }) {
+export default function AdminRoute({ children }) {
   const { state } = useContext(Store);
   const { userInfo } = state;
-  return userInfo ? children : <Navigate to="/signin" />;
+  return userInfo && userInfo.isAdmin ? children : <Navigate to="/signin" />;
 }
